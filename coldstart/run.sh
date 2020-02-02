8a0ffbbb80baf67332c89870dfa80d8f218c7c88
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-02-02 14:18:36 -0800

    Improvements

--- coldstart/run.sh
+++ coldstart/run.sh
@@ -7,10 +7,10 @@ result_file=$4
 
 if [ "$run_type" == "baseline" ]
 then
-  ext_version=disabled
+  java_tool_options=
   agent_present=false
 else
-  ext_version=~2
+  java_tool_options=-javaagent:/home/agent.jar
   agent_present=true
 fi
 
@@ -23,16 +23,16 @@ start_time="$(date -u +%s.%3N)"
 
 az webapp config appsettings set --name $app_name \
                                  --resource-group $app_name \
-                                 --settings ApplicationInsightsAgent_EXTENSION_VERSION=$ext_version UNIQUE_START_ID=$unique_start_id
+                                 --settings JAVA_TOOL_OPTIONS=$java_tool_options UNIQUE_START_ID=$unique_start_id
 
-until curl -s http://$app_name.azurewebsites.net/unique-start-id | grep -q "Unique start id: $unique_start_id"
+until [[ $(curl -s http://$app_name.azurewebsites.net/unique-start-id) == $unique_start_id ]]
 do
   sleep 1
 done
 
 end_time="$(date -u +%s.%3N)"
 
-# fail if app is not in the correct state
-curl -s http://$app_name.azurewebsites.net/Home/About | grep -q "ApplicationInsights agent is present: $agent_present"
+# fail if agent is not in the correct state
+[[ $(curl -s http://$app_name.azurewebsites.net/agent-check) == $agent_present ]]
 
 echo "$end_time - $start_time" | bc >> $result_file
